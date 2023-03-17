8e91b761d40d113dc1a7f345c242df85e5ece2ae
Jonathan Cardoso-Silva <jonathan.car.silva@gmail.com>
2023-03-17 15:40:03 +0000

    Added get_all_headlines_as_df to utils.py. Addresses #2

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,4 +1,18 @@
+import requests
+
 import pandas as pd
+
+from bs4 import BeautifulSoup
+
+def get_all_headlines_as_df(wiki_url): 
+    # Code from @tz1211
+    response = requests.get(wiki_url)
+    soup = BeautifulSoup(response.text, 'html.parser') 
+    headlines_soup = soup.find_all(attrs={'class': 'mw-headline'}) 
+    df = get_headlines_as_df(headlines_soup) 
+
+    return df 
+
 
 def get_headlines_as_df(headlines_soup):
     
